99f028103f87ae146d524dc25fe019a1e9d7cc8c
Sinomar <sinomarjunior97@gmail.com>
2023-08-29 17:17:43 -0300

    ERRO

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Digite o Numero da sua Conta:");
+            int Numero = scanner.nextInt();
+
+        System.out.println("Digite a sua Agencia:");
+            String Agencia = scanner.next();
+
+        System.out.println("Digite seu Saldo:");
+            Float Saldo = scanner.nextFloat();
+
+        System.out.println("Digite seu Nome:");
+            String Nome = scanner.next();
+
     }
 }
