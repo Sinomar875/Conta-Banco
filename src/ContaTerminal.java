31e26fb2f8f625cb1188186c36b962f201acf3b3
Sinomar <sinomarjunior97@gmail.com>
2023-08-30 10:48:28 -0300

    finalizado

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -6,7 +6,7 @@ public class ContaTerminal {
         Scanner scanner = new Scanner(System.in);
         
         System.out.println("Digite o Numero da sua Conta:");
-            int Numero = scanner.nextInt();
+            int Conta = scanner.nextInt();
 
         System.out.println("Digite a sua Agencia:");
             String Agencia = scanner.next();
@@ -16,6 +16,8 @@ public class ContaTerminal {
 
         System.out.println("Digite seu Nome:");
             String Nome = scanner.next();
+
+            System.out.println("Ola " + Nome + ", obrigado por criar uma conta em nosso banco, sua agencia e " + Agencia + ", conta " + Conta + " e seu saldo " + Saldo + " ja esta disponivel para saque.");
 
     }
 }
